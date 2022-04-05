bdb752183d37d22472d597c36a581dc5ae066e3d
GeneralMine <marvin@raiser.dev>
2022-04-06 00:24:34 +0200

    equalize both sides with prettier code

--- python2.py
+++ python2.py
@@ -7,21 +7,23 @@ import os
 import time
 import json
 
-pipe_name_command = "~/pipe_test_command"
-pipe_name_telemetry = "~/pipe_test_telemetry"
+pipe_name_command = "/tmp/pipe_test_command"
+pipe_name_telemetry = "/tmp/pipe_test_telemetry"
 telemetry = json.load(open("telemetry.json"))
-
-# Command: Read Pipe
-if not os.path.exists(pipe_name_command):
-    os.mkfifo(pipe_name_command)
-commandPipe = open(pipe_name_command, "r")
+telemetryCounter = 0
 commandCounter = 0
 
-# Telemetry: Write Pipe
+# Create Pipes if not exists
+if not os.path.exists(pipe_name_command):
+    os.mkfifo(pipe_name_command)
 if not os.path.exists(pipe_name_telemetry):
     os.mkfifo(pipe_name_telemetry)
+
+# Command: Read Pipe
+commandPipe = open(pipe_name_command, "r")
+
+# Telemetry: Write Pipe
 telemetryPipe = os.open(pipe_name_telemetry, os.O_WRONLY)
-telemetryCounter = 0
 
 # Loop
 while True:
@@ -30,12 +32,12 @@ while True:
     msg = (json.dumps(telemetry) + "\n").encode("utf-8")
     os.write(telemetryPipe, msg)
     print("[Sending][Number %04d] Telemetry to Python3\n" % telemetryCounter)
-    telemetryCounter = telemetryCounter + 1
+    telemetryCounter += 1
 
     # Command: Read
     line = json.loads(commandPipe.readline()[:-1])
-    print("[Receive][Number %04d] Command from Python3: %s\n" % (commandCounter, line["type"]))
-    commandCounter = commandCounter + 1
+    print("[Receive][Number %04d] Command from Python3: %s\n" % (commandCounter, line["type"] + ":" + line["payload"]))
+    commandCounter += 1
 
     # Delay
     time.sleep(1)
--- python3.py
+++ python3.py
@@ -7,60 +7,37 @@ import os
 import time
 import json
 
-pipe_name_command = "~/pipe_test_command"
-pipe_name_telemetry = "~/pipe_test_telemetry"
-commands = [
-    {
-        "channel": "function",
-        "type": "aquire",
-        "payload" : 1
-    },
-    {
-        "channel": "control",
-        "type": "takeoff",
-        "payload" : 10
-    },
-    {
-        "channel": "control",
-        "type": "move",
-        "payload" : [1,2,3]
-    },
-    {
-        "channel": "control",
-        "type": "land",
-        "payload" : 0
-    },
-    {
-        "channel": "function",
-        "type": "free",
-        "payload" : 0
-    }
-]
-
-# Command: Write Pipe
-if not os.path.exists(pipe_name_command):
-    os.mkfifo(pipe_name_command)
-commandPipe = os.open(pipe_name_command, os.O_WRONLY)
+pipe_name_command = "/tmp/pipe_test_command"
+pipe_name_telemetry = "/tmp/pipe_test_telemetry"
+commands = json.load(open("commands.json"))
+telemetryCounter = 0
 commandCounter = 0
 
-# Telemetry: Read Pipe
+# Create Pipes if not exists
+if not os.path.exists(pipe_name_command):
+    os.mkfifo(pipe_name_command)
 if not os.path.exists(pipe_name_telemetry):
     os.mkfifo(pipe_name_telemetry)
+
+# Command: Write Pipe
+commandPipe = os.open(pipe_name_command, os.O_WRONLY)
+
+# Telemetry: Read Pipe
 telemetryPipe = open(pipe_name_telemetry, "r")
-telemetryCounter = 0
 
 # Loop
 while True:
     # Telemetry: Read
     line = json.loads(telemetryPipe.readline()[:-1])
     print("[Receive][Number %04d] Telemetry from Python2: %s\n" % (telemetryCounter, line["debug"]))
-    telemetryCounter = telemetryCounter + 1
+    telemetryCounter += 1
 
     # Command: Write
+    commands[1]["payload"] = str(commandCounter)
     msg = (json.dumps(commands[commandCounter % len(commands)]) + "\n").encode("utf-8")
     os.write(commandPipe, msg)
     print("[Sending][Number %04d] Command to Python2\n" % commandCounter)
-    commandCounter = commandCounter + 1
+    commandCounter += 1
 
     # Delay
     time.sleep(1)
